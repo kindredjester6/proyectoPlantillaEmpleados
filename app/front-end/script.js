8f01124ceff84199c764ff300bccc98de7395a5c
dayanaxie <dxie@estudiantec.cr>
2024-08-14 20:12:39 -0600

    estructura web

---
+++ app/front-end/script.js
@@ -0,0 +1,35 @@
+data = {id: 1, empleado: "jimin", salario: 4000};
+
+
+function regresarInicio(){
+    location.href = "inicial.html";
+
+}
+
+// funcion que valida que el campo del nombre y del salario no esten vacios
+function validarVacio(){
+    var nombre = document.forms["form"]["Nombre"].value;
+    var salario = document.forms["form"]["Salario"].value;
+
+    if (nombre == ""){
+        alert("Por favor ingrese el nombre del empleado");
+    } if(salario == ""){
+        alert("Por favor ingrese el salario del empleado");
+    } else{
+        revisarInput(nombre, salario);
+    };
+}
+
+
+function revisarInput(nombre, salario){
+    // expresion regular
+    let regexNom = /^[a-zA-Z-]+$/;
+    let regexNum = /^[0-9]+$/;
+    if(!regexNom.test(nombre)){
+        alert("El nombre solo puede tener letras del alfabeto o guion");
+    } if(!regexNum.test(salario)){
+        alert("El salario solo puede tener valores numericos");
+    };
+}
+
+
